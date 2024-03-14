092f24530f977e48602e43ccafa5aee835a5978b
방소영 <bjo8244@naver.com>
2024-03-14 14:11:32 +0900

    [Feat] 탈퇴여부기반 회원조회기능 구현
    Former-commit-id: a1ff0e30bf7b48bba2a48fadf7fe932b878ddc0f

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.b209.hansotbab.user;
-
-import com.b209.hansotbab.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
